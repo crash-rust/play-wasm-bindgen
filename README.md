a4a545074617b763a8bc51e0c464f74afaefdd43
jaylenchan <jaylen.work@hotmail.com>
2022-10-20 19:31:35 +0800

    feat: finish Conway's Game of Life

---
+++ README.md
@@ -0,0 +1,5 @@
+## 环境准备
+
+1. 安装wasm-pack: `curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh`
+2. 安装cargo-generate: `cargo install cargo-generate`
+3. 安装npm: `npm install npm@latest -g`
---
+++ www/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bootstrap.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, '..')
+    })
+  ],
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    }
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true
+  }
+};
